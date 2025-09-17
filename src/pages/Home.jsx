ecaa250a0f48d3571fd6f875e1d58d89e91c6562
ficusreligiosa <manavsingh89777@gmail.com>
2025-09-17 10:37:45 +0530

    Updated portfolio with small change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,13 @@
-import { Navbar } from "../components/Navbar";
-import { ThemeToggle } from "../components/ThemeToggle";
-import { StarBackground } from "@/components/StarBackground";
-import { HeroSection } from "../components/HeroSection";
-import { AboutSection } from "../components/AboutSection";
-import { SkillsSection } from "../components/SkillsSection";
-import { ProjectsSection } from "./components/ProjectsSection";
-import { ContactSection } from "../components/ContactSection";
-import { Footer } from "../components/Footer";
+import { Navbar } from "../Components/Navbar";
+import { ThemeToggle } from "../Components/ThemeToggle";
+import { StarBackground } from "../Components/StarBackground";
+import { HeroSection } from "../Components/HeroSection";
+import { AboutSection } from "../Components/AboutSection";
+import { SkillsSection } from "../Components/SkillsSection";
+import { ProjectsSection } from "../Components/ProjectsSection";
+import { ContactSection } from "../Components/ContactSection";
+import { Footer } from "../Components/Footer";
+
 
 export const Home = () => {
   return (
