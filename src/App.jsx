d41ee5a13805dd962df87fdcfd0d793facce693c
ficusreligiosa <manavsingh89777@gmail.com>
2025-09-17 11:43:19 +0530

    Updated portfolio with small change

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home";       // ✅ default import
 import NotFound from "./pages/NotFound"; // also switch to default export for consistency
 
 function App() {
   return (
     <>
-      <BrowserRouter basename="/">
+      <HashRouter>
         <Routes>
           <Route index element={<Home />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   );
 }
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import path from "path"
+import path from 'path'
 
 export default defineConfig({
-  base: './', // ← crucial for Netlify deployment
+  base: '/my-portfolio/', // <-- replace with your GitHub repo name
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
