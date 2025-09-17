831738abb6f940a72b7783bd424f3836e29a4383
ficusreligiosa <manavsingh89777@gmail.com>
2025-09-17 11:11:46 +0530

    Updated portfolio with small change

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import NotFound from "./pages/NotFound"; // also switch to default export for co
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename="/">
         <Routes>
           <Route index element={<Home />} />
           <Route path="*" element={<NotFound />} />
