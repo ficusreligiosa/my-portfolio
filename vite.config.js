b72e4bdb8a4c05d226fb7eec2d5e1a8ee6afef6b
ficusreligiosa <manavsingh89777@gmail.com>
2025-09-17 11:06:05 +0530

    Updated portfolio with small change

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import path from "path"
 
 export default defineConfig({
-  base: './', // <-- important for Netlify
+  base: './', // ← crucial for Netlify deployment
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
